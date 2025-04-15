a7ee54ad48a471fe64d6e7601924597e9c7da262
varun choudhry <varun.choudhry3@gmail.com>
2025-04-15 18:29:51 +0530

    added some notes for later use

--- ui.py
+++ ui.py
@@ -98,5 +98,11 @@ def sentence_split(text: str):
     sentences = sentence_end.split(text)
     return [s.strip() for s in sentences if s.strip()]
     
-    
-    
+#Potential chunking implementation which leverages the insights provided by Azure Document Intelligence to create chunks which could better preserve logical groupings
+def azure_chunker()   
+
+#Azure DI has a dict of keys paragraphs, sections and tables, in context of this service paragraphs can also mean words, its the smallest unit of classification in Azure DI when it comes to how sections are mapped. Secttions have a list called elements which represent references to other sections/paragraphs as strings like "/sections/10", "/paragraphs/25" or "/tables/3". It also returns the whole document as a string
+#Each of these groupings have an offset and length attached to them, which could be used to simple split the text string into groups. For example, chunk 1 could be text[<offset of section1>:offset+ length]. Due to the hiearchical nature of sections, need to find a better way which avoids redundancies.  
+
+#possible solution, there are some 'paragraphs' which have a role in them, like title, section heading, the offset of these paragraphs could be use to as a breakpoint without having to traverse sections which is closer to a tree and would be difficult to flatten to something suitable for vector embeddings without making it unneccesarily complex.
+#Need to analyze how tables structures would fit into this
