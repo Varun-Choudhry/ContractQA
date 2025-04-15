c4318636e181c504d2f5bc3fea83cd10b83c0653
varun choudhry <varun.choudhry3@gmail.com>
2025-04-15 17:39:06 +0530

    added a basic semantic chunking method

--- ui.py
+++ ui.py
@@ -28,6 +28,8 @@ upload_result = upload(chunks, "weaviate")
 def chunker(text, size, overlap, mode):
         if mode == "fixed":
             return fixed_size_chunker(text, size, overlap)
+        elif mode == "semantic"
+            return breakpoint_semantic_chunker(text)
         return
 
 def extract_content(uploaded_file, tool):
@@ -46,6 +48,11 @@ def get_embedding(text:str):
     #placeholder
     return ""
 
+def get_embedding_batch(texts: list):
+    #placeholder
+    return [""]
+
+#Need to decouple weaviate data object into a separate function
 def fixed_size_chunker(text , chunk_size: int = 256, overlap: int = 20):    
     encoder = tiktoken.encoding_for_model("gpt-4o")
     token_text = encoder.encode(text)
@@ -62,4 +69,34 @@ def fixed_size_chunker(text , chunk_size: int = 256, overlap: int = 20):
         chunks.append(chunk_object)
         chunk_count += 1
     return chunks
+
+
+# Current implementation is very basic and inefficient. Will consider a two pointer approach to improve it.
+# Need to identify a way to derive an acceptable cosine similarity threshold from the document itself for improved performance
+# Since the data has been broken down into sentences, could possibly add sentence overlapping 
+#PENDING TESTING
+def breakpoint_semantic_chunker(text):
+    from sklearn.metrics.pairwise import cosine_similarity
+    chunks = []
+    sentences = sentence_split(text)
+    vectors = get_embedding_batch(sentences)
+    current_chunk = [sentences[0]]
+    for i in range(1,len(sentences)):
+        similarity = cosine_similarity([vectors[i-1]], [vectors[i]])
+        if similarity[0][0] <= 0.6:
+            chunks.append(" ".join(current_chunk))
+            current_chunk=[sentences[i]]
+        else:
+            current_chunk.append(sentences[i])
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))                    
+    return chunks
+    
+#Using a basic sentence split using regex for now   
+def sentence_split(text: str):
+    sentence_end = re.compile(r'(?<=[.!?])\s+(?=[A-Z])')
+    sentences = sentence_end.split(text)
+    return [s.strip() for s in sentences if s.strip()]
+    
+    
     
